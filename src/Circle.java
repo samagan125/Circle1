9a70b8ef833c00fc7a8cfc5336ea7206c5aa19cf
Admin <samagantolonbayev@gmail.com>
2023-07-19 16:31:41 +0300

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,16 @@
+public class Circle {
+    private static final double PI = 3.14;
+    private static double radius;
+
+    static double area() {
+        return PI * (radius * radius);
+    }
+
+    static double circumference() {
+        return PI * 2 * radius;
+    }
+
+    public static void setRadius(double radius) {
+        Circle.radius = radius;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle.setRadius(12.3);
+        System.out.println(Circle.area());
+        System.out.println(Circle.circumference());
+    }
+}
